9bd5843c116150d14b3f6a3279d0fb284b997fda
Jonathan Curtis <mrjcurtisdev@gmail.com>
2026-02-08 10:03:45 -0500

    chore: add test project & scripts

--- addons/nodetunnel/autoloads/nt_room.gd
+++
@@ -1,8 +0,0 @@
-class_name NodeTunnelRoom
-
-var room_id: String
-var room_metadata: String
-
-func _init(id: String, metadata: String) -> void:
-	room_id = id
-	room_metadata = metadata
---
+++ scripts/test.bat
@@ -0,0 +1,9 @@
+@echo off
+cd ..
+cargo build -p nt_client
+if not exist "addons\nodetunnel\bin" mkdir "addons\nodetunnel\bin"
+if exist target\debug\nt_client.dll (
+    copy /Y target\debug\nt_client.dll addons\nodetunnel\bin\
+)
+godot --path "./gdtest" --mode=host
+godot --path "./gdtest" --mode=join
